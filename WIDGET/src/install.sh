ff2d1d41cba0566fe3c26a8882d2ac53b7733d5c
Justin Kennedy <jekennedy@umass.edu>
2016-02-18 12:04:04 -0500

    install.sh manually adds ssl certs to /etc/ssl/certs if they don't already exist

--- WIDGET/src/install.sh
+++ WIDGET/src/install.sh
@@ -1,4 +1,13 @@
-#Run this after pulling 
+#Run this after pulling
+CERT = "/etc/ssl/certs/cert.pem"
+KEY = "/etc/ssl/certs/key.pem"
+
+if [ -e $CERT ] && [ -e $KEY ];then
+  echo "Certs already exist"
+else
+  echo "Creating New Certs"
+  openssl req -x509 -newkey rsa:2048 -keyout /etc/ssl/certs/key.pem -out /etc/ssl/certs/cert.pem -days 100
+fi
 
 sudo cp widget_client.py /usr/local/bin
 sudo chmod +x /usr/local/bin/widget_client.py
