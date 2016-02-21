94baa6af1e2f0784c68a5f579cdf1b638a277be3
romanganchin <umass@beaglebone.mshome.net>
2016-02-21 19:20:28 +0000

    i2c bus 0/1 healthCheck w/ reboot functionality

--- DEFENSE/healthcheck.sh
+++ DEFENSE/healthcheck.sh
@@ -2,19 +2,31 @@
 
 clear
 
-echo "The script starts now"
 touch file2
 touch file3
+
+#Add data of i2c mapping to file2 for busses 0 and 1, ignoring 2
 i2cdetect -y -r 0 > file2
 i2cdetect -y -r 1 >> file2
-#echo "gibberish" >> file2 just to know that when it is different it works
+
+#Attempt at enabling and transferring map for bus 2
+#if [ ! -f /sys/devices/bone_capemgr.9/slots 
+#	echo "[8BWh!t3]" | sudo -S [ echo BB-I2C1 > /sys/devices/bone_capemgr.9/slots ]
+#fi
+#i2cdetect -y -r 2 >> file2
+
+#A test to make sure the program works on positive diff
+#echo "gibberish" >> file2
+
+#Add data of what is different between what is expected and what is found to file 3
 diff -u results.txt file2 > file3
 
+#If there is a difference between what is expected and what is found, reboot
 if [ -s file3 ]
 then
    cat file3
-   echo "file exists and not empty then something is wrong do not boot"
+   printf "\nSystem has different setup, do not boot\n\n"
+	echo "[8BWh!t3]" | sudo -S reboot
 else
-   echo "File exists but empty everything is safe"
+   printf "\nEverything is safe :)\n\n"
 fi
-
